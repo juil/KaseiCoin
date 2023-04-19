fbe5ff2577df0b6bc98703a0efdff4528df83c6a
Juil <dev@juil.me>
2023-04-19 10:33:18 -0400

    Update solidity compiler ver to 0.8 and add license

--- KaseiCoinCrowdsale.sol
+++ KaseiCoinCrowdsale.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 import "./KaseiCoin.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/Crowdsale.sol";
